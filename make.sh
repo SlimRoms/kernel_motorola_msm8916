451b34dc35cbf4be25471de64c98ca359325cf64
Josue Rivera <prbassplayer@gmail.com>
2015-07-01 20:25:31 +0200

    msm8916: Add simple script
    Change-Id: Ice924e1a506c4f39fe9746f26a598d007ca5296b
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

---
+++ make.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Stupid shell script to compile kernel, nothing fancy
+
+# Exports all the needed things Arch, SubArch and Cross Compile
+export ARCH=arm
+echo 'exporting Arch'
+export SUBARCH=arm
+echo 'exporting SubArch'
+export CROSS_COMPILE=/home/prbassplayer/Slim7/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+echo 'exporting Cross Compile toolchain (Googles 4.9 arm-linux-androideabi-)'
+
+# Make sure build is clean!
+echo 'Cleaning build'
+make clean
+
+# Generates a new .config and exist
+if [ "$1" = "config" ] ; then
+    echo 'Making defconfig for slim_surnia_defconfig'
+    make slim_surnia_defconfig
+    exit
+fi
+
+# Lets go!
+echo 'Lets start!'
+make -j$1
